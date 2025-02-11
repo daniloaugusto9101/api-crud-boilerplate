4605b1cf4840ae27160c1eb8c1e584197f06c175
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-02-10 21:07:26 -0300

    feat: Add variáveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`🔥Server is running on port http://localhost:${port}`);
+});
