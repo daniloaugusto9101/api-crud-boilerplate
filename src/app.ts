0b1f6b021a0ec81795e921e217a7d97f8bb0a612
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-02-10 21:49:41 -0300

    feat: Update camada rotas

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import router from "./routes/routesV1";
+import routerV1 from "./routes/routesV1";
 
 function createApp() {
   const app = express();
 
   app.use(express.json());
 
-  app.use("/api", router);
+  app.use("/api/v1.0", routerV1);
 
   return app;
 }
