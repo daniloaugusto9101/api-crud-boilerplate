b673323848b35d4f586935a15ee2df75b7799ed8
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-02-13 20:51:32 -0300

    feat: Add camada utils

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { getUsersService } from "../services/users-service";
+import { ok } from "../utils/http-helper";
 
 export const getUsers = async (req: Request, res: Response) => {
   const data = await getUsersService();
-  res.status(200).json(data);
+  const response = await ok(data);
+  res.status(response.statusCode).json(response.body);
 };
