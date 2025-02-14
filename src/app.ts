c42494daaa766bf8dd19f39edf0007326710282f
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-02-13 21:28:05 -0300

    feat: Add camada cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import routerV1 from "./routes/routesV1";
+import cors from "cors";
 
 function createApp() {
   const app = express();
@@ -7,6 +8,8 @@ function createApp() {
   app.use(express.json());
 
   app.use("/api/v1.0", routerV1);
+
+  app.use(cors());
 
   return app;
 }
